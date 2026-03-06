650d51c83bf64dac4178bf547eac4dee8a7129e6
KwachOjunga <ojungakwach@gmail.com>
2026-03-06 16:10:22 +0300

    modify header component to behave in change of theme

--- src/components/header.rs
+++ src/components/header.rs
@@ -7,7 +7,7 @@ pub fn Header() -> impl IntoView {
     view! {
         <header class="fixed top-0 left-0 right-0 z-50 glass-effect backdrop-blur-md">
             <div class="max-w-4xl mx-auto px-4 py-4 flex justify-between items-center">
-                <div class="font-semibold text-lg">Reginald Ojunga</div>
+                <div class="font-semibold bg-[#fdfdfd] text-black dark:bg-[#0a0a0a] dark:text-white text-lg">Reginald Ojunga</div>
 
                 <ThemeToggle />
             </div>
